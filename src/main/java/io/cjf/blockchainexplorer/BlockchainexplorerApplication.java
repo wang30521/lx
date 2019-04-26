15783601ea4a9eb4eab4aeda8bfde43fd30fb080
王攀 <3052124905@qq.com>
2019-04-26 19:23:44 +0800

    first init

---
+++ src/main/java/io/cjf/blockchainexplorer/BlockchainexplorerApplication.java
@@ -0,0 +1,13 @@
+package io.cjf.blockchainexplorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockchainexplorerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlockchainexplorerApplication.class, args);
+    }
+
+}
